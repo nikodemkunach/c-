271875cd3f2d5d9d789687f5db6bc3c1988cb629
nikodemkunach <91730808+nikodemkunach@users.noreply.github.com>
2022-01-13 18:55:23 +0100

    Update Time.h

--- include/Time.h
+++ include/Time.h
@@ -1,29 +1,22 @@
-#include <iostream>
-#include <string>
-
-namespace s25030 {
-    struct Time {
-        int hh;
-        int mm;
-        int ss;
-        
-        Time(int, int, int);
+struct Time
+{
+    int hour=1, minutes=0, sec=0;
+    std::string to_string();
+    auto next_hour()->void;
+    auto next_minute()->void;
+    auto next_second()->void;
+    Time(int,int,int);
     
-    auto next_hour() -> void;
-    auto next_minute() -> void;
-    auto next_second() -> void;
+    auto time_of_day() const;
+    
+    auto operator+ (Time const&)  const -> Time;
+    auto operator- (Time const&)  const -> Time;
+    auto operator< (Time const&)  const -> bool;
+    auto operator> (Time const&)  const -> bool;
+    auto operator== (Time const&) const -> bool;
+    auto operator!= (Time const&) const -> bool;
+};
 
-    enum class Time_of_day {morning, day, evening, night};
-    static auto to_string(Time_of_day const) -> std::string;
-    auto time_of_day() const -> Time_of_day;
-    auto to_string() const -> std::string;
+enum class Time_of_day {rano, dzien, wieczor, noc};
 
-    auto operator+ (Time const&) -> Time;
-    auto operator- (Time const&) -> Time;
-    auto operator< (Time const&) -> bool;
-    auto operator> (Time const&) -> bool;
-    auto operator== (Time const&) -> bool;
-    auto operator!= (Time const&) -> bool;
-
-    };
-}
+auto to_string(Time_of_day) -> std::string;
